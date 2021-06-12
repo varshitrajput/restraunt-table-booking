039382ddce45a6ff2c9d36948a0fb2769e473f56
varshitrajput <vsrfiles@gmail.com>
2021-06-13 01:32:53 +0530

    asd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 //mongoose connection
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
   });
 
