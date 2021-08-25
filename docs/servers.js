7d15ee5325b820288eda034c392140934da070a2
varshitrajput <varshit@classplus.co>
2021-08-26 02:11:21 +0530

    fi

--- docs/servers.js
+++ docs/servers.js
@@ -1,7 +1,7 @@
 module.exports = {
     servers:[
         {
-            url:"http://localhost:3030",
+            url:"https://intense-wave-33897.herokuapp.com",
             description:"Local server, npm run dev"
         },
     ]
